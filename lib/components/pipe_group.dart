cfcfb5ca7bc1b57850208b52a52278c77cbbd34d
RafaelMagnago <rmagnago@outlook.com>
2025-12-01 19:35:28 -0300

    randomizacao dos tubos

--- lib/components/pipe_group.dart
+++ lib/components/pipe_group.dart
@@ -1,20 +1,28 @@
+import 'dart:math';
+
 import 'package:flame/components.dart';
 import 'package:flap_bird/components/pipe.dart';
 import 'package:flap_bird/game/conifguration.dart';
 import 'package:flap_bird/game/flappy_bird_game.dart';
 import 'package:flap_bird/game/pipe_position.dart';
+import 'package:flutter/widgets.dart';
 
-class PipeGroup extends PositionComponent
-    with HasGameRef<FlappyBirdGame> {
+class PipeGroup extends PositionComponent with HasGameRef<FlappyBirdGame> {
   PipeGroup();
+
+  final _random = Random();
 
   @override
   Future<void> onLoad() async {
     position.x = gameRef.size.x;
 
+    final heightMinusGround = gameRef.size.y - Config.groundHeight;
+    final spacing = 100 + _random.nextDouble() * (heightMinusGround / 4);
+    final centerY = spacing + _random.nextDouble() * (heightMinusGround - spacing);
+
     addAll([
-      Pipe(height: 100, pipePosition: PipePosition.top),
-      Pipe(height: 200, pipePosition: PipePosition.bottom),
+      Pipe(height: centerY - spacing / 2, pipePosition: PipePosition.top),
+      Pipe(height: heightMinusGround - (centerY + spacing / 2), pipePosition: PipePosition.bottom),
     ]);
   }
 
@@ -22,5 +30,10 @@ class PipeGroup extends PositionComponent
   void update(double dt) {
     super.update(dt);
     position.x -= Config.gameSpeed * dt;
+
+    if (position.x < -10) {
+      removeFromParent();
+      debugPrint('Pipe removido');
+    }
   }
 }
--- lib/game/flappy_bird_game.dart
+++ lib/game/flappy_bird_game.dart
@@ -1,19 +1,27 @@
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flap_bird/components/background.dart';
 import 'package:flap_bird/components/ground.dart';
 import 'package:flap_bird/components/bird.dart';
 import 'package:flap_bird/components/pipe_group.dart';
+import 'package:flap_bird/game/conifguration.dart';
 
 class FlappyBirdGame extends FlameGame {
   late Bird bird;
+  Timer interval = Timer(Config.pipeInterval, repeat: true);
 
   @override
   Future<void> onLoad() async {
-    addAll([
-      Background(),
-      Ground(),
-      bird = Bird(),
-      PipeGroup(),
-      ]);
+    addAll([Background(), Ground(), bird = Bird(), PipeGroup()]);
+
+    interval.onTick = () {
+      add(PipeGroup());
+    };
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    interval.update(dt);
   }
 }
